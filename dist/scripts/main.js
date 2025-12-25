eaf640df5cb0590d4c33a209f9fc8cab475d3add
anasavlevich <ana.savlevich@gmail.com>
2025-12-25 20:31:34 +0700

    commit 19.5 - bug fixes

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -7,18 +7,32 @@
   });
 
   const eventPP = document.querySelector("#js-eventPP");
+
   if (eventPP) {
     const eventOpenBtn = document.querySelector("#js-eventOpenBtn");
-    eventOpenBtn.addEventListener("click", function () {
-      root.classList.add("show-event-popup");
-    });
-    eventPP.addEventListener("click", function (event) {
-      if (
-        event.target === this ||
-        event.target.classList.contains("js-ppCloseBtn")
-      ) {
+    const closeEventPP = function (event) {
+      function close() {
+        document.removeEventListener("keyup", closeEventPP);
+        eventPP.removeEventListener("click", closeEventPP);
         root.classList.remove("show-event-popup");
       }
+      switch (event.type) {
+        case "keyup":
+          if (event.key === "Escape") close();
+          break;
+        case "click":
+          if (
+            event.target === this ||
+            event.target.classList.contains("js-ppCloseBtn")
+          )
+            close();
+          break;
+      }
+    };
+    eventOpenBtn.addEventListener("click", function () {
+      root.classList.add("show-event-popup");
+      document.addEventListener("keyup", closeEventPP);
+      eventPP.addEventListener("click", closeEventPP);
     });
   };
 
